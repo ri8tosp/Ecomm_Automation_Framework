9b7a130e43b6337d07064bf14f27e97295756070
sj <ri8tosp@gmail.com>
2024-05-06 00:39:07 +0530

    Passing Data using HashMap instead of Object Array

--- src/test/java/ecommwebsite/tests/PlaceOrderFeatureTest.java
+++ src/test/java/ecommwebsite/tests/PlaceOrderFeatureTest.java
@@ -1,6 +1,7 @@
 package ecommwebsite.tests;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
@@ -18,22 +19,20 @@ import ecommwebsite.testComponents.BaseTest;
 
 public class PlaceOrderFeatureTest extends BaseTest {
 
-
-
 	@Test(dataProvider = "getData", groups = "Purchase")
-	public void placeOrderTestCase(String email, String pwd, String productName) throws IOException
+	public void placeOrderTestCase(HashMap <String, String > map) throws IOException
 
 	{
 
-		ProductCatalogue productCatalogue = landingPage.logIntoApp(email, pwd);
+		ProductCatalogue productCatalogue = landingPage.logIntoApp(map.get("email"), map.get("pwd"));
 
 		List<WebElement> products = productCatalogue.getProductList();
 
-		productCatalogue.addProductToCart(productName);
+		productCatalogue.addProductToCart(map.get("productName"));
 
 		MyCartPage cartPage = productCatalogue.goToCart();
 
-		Boolean isProductDisplayed = cartPage.isProductDisplayed(productName);
+		Boolean isProductDisplayed = cartPage.isProductDisplayed(map.get("productName"));
 
 		Assert.assertTrue(isProductDisplayed);
 
@@ -52,7 +51,7 @@ public class PlaceOrderFeatureTest extends BaseTest {
 
 	@Test(dependsOnMethods = { "placeOrderTestCase" })
 	public void validatingOrderHistoryPage() {
-		
+
 		String productName = "Zara Coat 3";
 
 		ProductCatalogue productCatalogue = landingPage.logIntoApp("rahulexample@abc.com", "ABCD123@abc");
@@ -69,7 +68,33 @@ public class PlaceOrderFeatureTest extends BaseTest {
 	public Object[][] getData()
 
 	{
+
+		// sending data using HashMap, this is because if there are more than 3
+		// parameters in placeOrderTestCase()
+		// then it will be very difficult to manage so many parameters in the method as
+		// a result better way to handle this is by HashMaP
+		
+		HashMap <String, String > map = new HashMap <String , String>();
+		
+	    map.put("email", "rahulexample@abc.com");
+	    map.put("pwd", "ABCD123@abc");
+	    map.put("productName", "ZARA COAT 3");
+	    
+	    
+		HashMap <String, String > map1 = new HashMap <String , String>();
+		
+	    map1.put("email", "harry@styles.com");
+	    map1.put("pwd", "ABCD123@abc");
+	    map1.put("productName", "IPHONE 13 PRO");
+	    
+	    return new Object[][] { {map},{map1} };
+	    
+	    
+	    /* using the Array way of DataProvider
+
 		return new Object[][] { { "rahulexample@abc.com", "ABCD123@abc", "ZARA COAT 3" },
 				{ "harry@styles.com", "ABCD123@abc", "IPHONE 13 PRO" } };
+				
+		*/		
 	}
 }
